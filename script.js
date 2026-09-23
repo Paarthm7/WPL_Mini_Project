2ac5fa8ca0ed9d8350ac19b456682d5e15e99dcd
divishaaaaa <manekdivisha@gmail.com>
2026-09-23 13:41:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function () {
+
+    setTimeout(function () {
+
+        const splash = document.getElementById("splashScreen");
+
+        if (splash) {
+            splash.classList.add("hide");
+        }
+
+    }, 2500);
+
+});
